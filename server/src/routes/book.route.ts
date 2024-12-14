a46d629b66a277847e2e98ee43b49b3f17528877
usman-faisal <usman.shamsi987@gmail.com>
2024-12-14 15:57:11 +0500

    routes for book controller

--- server/src/routes/book.route.ts
+++ server/src/routes/book.route.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { verifyAuth } from "../middlewares/auth.middleware";
-import { addBook } from "../controllers/book.controller";
+import { addBook, getBook, getBooks, getIssuedBooks, issueBook } from "../controllers/book.controller";
 import { upload } from "../services/storage.service";
 const router = Router();
 
-router.post('/book', verifyAuth(['admin']), upload.single("image"), upload.single("pdf"), addBook)
+router.get('/books', getBooks)
+router.get('/books/:id', getBook)
+router.post('/books/:id/issue',verifyAuth(['user']), issueBook)
+router.get('/issued-books', verifyAuth(['user']), getIssuedBooks)
+router.post('/books', verifyAuth(['admin']), upload.single("image"), upload.single("pdf"), addBook)
 
 export default router;
